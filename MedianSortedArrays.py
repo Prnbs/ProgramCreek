323d2bac25ffdbba8d455c108c135a646f2abe66
Pranabesh Sinha <prnbs@Pranabeshs-MacBook-Pro.local>
2015-10-06 07:27:37 -0700

    add + 1 to end

--- MedianSortedArrays.py
+++ MedianSortedArrays.py
@@ -19,7 +19,7 @@ def findMedian(arr1, arr1_start, arr1_end, arr2, arr2_start, arr2_end):
     if median1 > median2:
         # for arr1 new start and stop are
         if arr1_remaining > 2:
-            arr1_end = int((arr1_end + arr1_start)/2)
+            arr1_end = int((arr1_end + arr1_start)/2) + 1
         # for arr2 new start and stop are
         if arr2_remaining > 2:
             arr2_start = int((arr2_end + arr2_start)/2)
@@ -28,7 +28,7 @@ def findMedian(arr1, arr1_start, arr1_end, arr2, arr2_start, arr2_end):
         if arr1_remaining > 2:
             arr1_start = int((arr1_end + arr1_start)/2)
         if arr2_remaining > 2:
-            arr2_end = int((arr2_end + arr2_start)/2)
+            arr2_end = int((arr2_end + arr2_start)/2) + 1
         findMedian(arr1, arr1_start, arr1_end, arr2, arr2_start, arr2_end)
 
 
@@ -43,12 +43,14 @@ def verify(arr1, arr2):
         index = int(len(merged_arr)/2)
         item1 = merged_arr[index]
         item2 = merged_arr[index+1]
-        print (item1 + item2)/2
+        print "Averaged", (item1 + item2)/2
 
 
 if __name__ == '__main__':
     arr1 = [i for i in range(1, 40, 2)]
-    arr2 = [i for i in range(2, 40, 2)]
+    arr2 = [i for i in range(2, 80, 2)]
+    # arr1 = [1, 5, 7, 10, 13]
+    # arr2 = [11, 15, 23, 30, 45]
     print arr1
     print arr2
     findMedian(arr1, 0, len(arr1), arr2, 0, len(arr2))
