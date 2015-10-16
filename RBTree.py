b41a4ad9f3dc9eb318b373c1b27c5ab9bcb470d7
Pranabesh Sinha <pranabesh.sinha@gmail.com>
2015-10-16 13:15:02 -0700

    Left rotate complete

--- RBTree.py
+++ RBTree.py
@@ -11,7 +11,7 @@ class RBNode:
         self.self_location = direction
 
     def __str__(self):
-        return str(self.data) + self.colour
+        return str(self.data) + self.colour + "  P:" + str(self.parent) + "  L:" + str(self.self_location)
 
     __repr__ = __str__
 
@@ -38,38 +38,51 @@ class RBTree:
         if inserted:
             return curr_node.children[direction], direction
 
-    def restructure(self, curr_node, direction_of_incoming):
-        print "-->",curr_node
-        print "---->",curr_node.children[direction_of_incoming]
-        # left rotate if direction of incoming = 1
-        if direction_of_incoming:
-            self.rotate(curr_node, direction_of_incoming)
-        # right rotation
-        else:
-            self.rotate(curr_node, direction_of_incoming)
-
     def rotate_left(self, curr_node):
         parent = curr_node.parent
         if parent.parent is not None:
             grand_parent = parent.parent
-            if grand_parent.self_location is -1 and grand_parent.self_location == parent.self_location\
-                    and parent.self_location == curr_node.self_location:
-                parent.self_location = -1
-                self.root = parent
-            # swap colous
-            parent.colour, grand_parent.colour = grand_parent.colour, parent.colour
-            grand_parent.children[1] = parent.children[0]
-            parent.children[0] = grand_parent
-            # now invert self_locations of the above
-            if grand_parent.children[1] is not None:
-                grand_parent.children[1].self_location = not grand_parent.children[1].self_location
-            if parent.children[0] is not None:
-                parent.children[0].self_location = not parent.children[0].self_location
+            root_changed = False
+            if grand_parent.self_location is -1:
+                # grand parent is root
+                if grand_parent.children[1] is not None and \
+                                grand_parent.children[1] == parent and \
+                                parent.self_location == curr_node.self_location:
+                    # all in a line so root will change
+                    self.root = parent
+                    parent.parent = grand_parent.parent
+                    grand_parent.children[1] = parent.children[0]
+                    parent.children[0] = grand_parent
+                    grand_parent.parent = parent
+                    grand_parent.self_location = False
+                    # swap colours
+                    parent.colour, grand_parent.colour = grand_parent.colour, parent.colour
+                    return parent, curr_node
+            else:
+                # no root, but are they in line
+                if grand_parent.self_location == parent.self_location and \
+                                parent.self_location == curr_node.self_location:
+                    grand_parent.parent.children[grand_parent.self_location] = parent
+                    parent.parent = grand_parent.parent
+                    grand_parent.children[1] = parent.children[0]
+                    parent.children[0] = grand_parent
+                    grand_parent.parent = parent
+                    grand_parent.self_location = False
+                    # swap colours
+                    parent.colour, grand_parent.colour = grand_parent.colour, parent.colour
+                    return parent, curr_node
 
-            parent.self_location, grand_parent.self_location = grand_parent.self_location, parent.self_location
+            # Not in line
+            grand_parent.children[curr_node.parent.self_location] = curr_node
+            curr_node.self_location = parent.self_location
+            curr_node.parent = grand_parent
+            if curr_node.children[0] is not None:
+                curr_node.children[0].parent = parent
+            curr_node.children[0] = parent
+            parent.children[1] = curr_node.children[0]
+            parent.parent = curr_node
 
-
-        return parent, curr_node
+            return parent, curr_node
 
 
     def rotate_right(self, curr_node):
@@ -90,32 +103,17 @@ class RBTree:
                 parent.children[1].self_location = not parent.children[1].self_location
 
             parent.self_location, grand_parent.self_location = grand_parent.self_location, parent.self_location
-
-
         return parent, curr_node
-
-
-    def rotate(self, curr_node, direction):
-        curr_node.parent.children[direction] = curr_node.children[not direction]
-        curr_node.children[not direction] = curr_node.parent
-        # check for root
-        if curr_node.parent.parent is not None:
-            curr_node.parent.parent.children[direction] = curr_node
-            curr_node.colour = "B"
-        else:
-            # For root
-            self.root = curr_node
-            self.root.colour = "B"
 
     def insert(self, item, curr_node):
         inserted_at, direction = self.insert_as_bst(item, curr_node)
         if direction is -1:
             return
-        while inserted_at.self_location is not -1 and inserted_at.colour is "R":
+        while inserted_at is not None and inserted_at.self_location is not -1 and inserted_at.colour is "R":
             print "Parent:", inserted_at
             print "     Node:",  inserted_at.children[direction]
             # inserted_at is the parent of node just inserted
-            if inserted_at.parent.colour is "R":
+            if inserted_at.parent is not None and inserted_at.parent.colour is "R":
                 parent = inserted_at.parent
                 sibling = parent.children[not direction]
                 uncle = parent.parent.children[not parent.self_location]
@@ -123,7 +121,7 @@ class RBTree:
                 # uncle rule takes precedence
                 if parent.colour is "R" and uncle is not None and uncle.colour is "R":
                     # push blackness down from grandparent
-                    if uncle is not None:
+                    if uncle is not None and uncle.parent is not None:
                         uncle.colour = uncle.parent.colour
                     parent.colour = parent.parent.colour
                     parent.parent.colour = "R"
@@ -147,7 +145,6 @@ class RBTree:
                             inserted_at, somm = self.rotate_right(inserted_at)
                         continue
 
-
                 inserted_at = parent.parent
                 node = None
                 node_parent = None
@@ -169,20 +166,20 @@ class RBTree:
 
 if __name__ == '__main__':
     rbTree = RBTree()
-    rbTree.insert(11, rbTree.root)
-    rbTree.insert(2, rbTree.root)
-    rbTree.insert(14, rbTree.root)
-    rbTree.insert(15, rbTree.root)
-    rbTree.insert(1, rbTree.root)
-    rbTree.insert(7, rbTree.root)
-    rbTree.insert(5, rbTree.root)
-    rbTree.insert(8, rbTree.root)
-    rbTree.insert(4, rbTree.root)
-    # rbTree.insert(20, rbTree.root)
     # rbTree.insert(10, rbTree.root)
-    # rbTree.insert(20, rbTree.root)
     # rbTree.insert(30, rbTree.root)
+    # rbTree.insert(40, rbTree.root)
+    # rbTree.insert(50, rbTree.root)
+    # rbTree.insert(60, rbTree.root)
 
+    rbTree.insert(10, rbTree.root)
+    rbTree.insert(5, rbTree.root)
+    rbTree.insert(6, rbTree.root)
+
+    # list_of_items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    #
+    # for data in list_of_items:
+    #     rbTree.insert(data, rbTree.root)
 
     print "end"
 
