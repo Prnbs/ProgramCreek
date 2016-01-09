8689c8f1dba9db2df61a4deb0cf21f2117f95f75
Pranabesh Sinha <pranabesh.sinha@gmail.com>
2016-01-08 17:33:02 -0800

    complex whiles

--- test_file.py
+++ test_file.py
@@ -16,9 +16,23 @@
 
 a = 1
 b = 8
-while b > 4:
+c = 8
+d = 10
+while b > 3:
     a += 1
     b -= 1
+    while c > 4:
+        b -= 2
+        c -= 1
+        while d > 4:
+            b -= 1
+            d -= 1
+
+print(a,b, c, d)
+
+
+
+
 
 
 
--- vm.py
+++ vm.py
@@ -15,38 +15,75 @@ class Globals:
         self.name = "Global"
 
 class PyByteVM:
-    def __init__(self, module, globals_object):
-        self.constants = module.co_consts
-        self.names = module.co_names
-        self.program = module.co_code
-        self.nlocals = module.co_nlocals
-        self.ip = 0
-        self.varnames = module.co_varnames
-        self.argcount = module.co_argcount
-        self.stack = []
-        self.globals = globals_object
+    def __init__(self, module=None, globals_object=None, parent=None):
+        if parent is None:
+            self.constants = module.co_consts
+            self.names = module.co_names
+            self.program = module.co_code
+            self.nlocals = module.co_nlocals
+            self.ip = 0
+            self.varnames = module.co_varnames
+            self.argcount = module.co_argcount
+            self.stack = []
+            self.globals = globals_object
+            self.parent = None
+        else:
+            self.constants = parent.constants
+            self.names = parent.names
+            self.program = parent.program
+            self.nlocals = parent.nlocals
+            self.ip = parent.ip
+            self.varnames = parent.varnames
+            self.argcount = parent.argcount
+            self.stack = []
+            self.globals = parent.globals
+            self.parent = parent
+        self.context = None
+        self.var_contexts = {}
 
     # Pushes co_consts[consti] onto the stack.
     def invoke_LOAD_CONST(self, val):
         self.stack.append(self.constants[val])
 
     def invoke_LOAD_GLOBAL(self, val):
-        self.stack.append(getattr(self.globals, self.names[val]))
+        self.stack.append(self.get_from_exec_frame(self.globals, self.names[val]))
 
+    def set_in_exec_frame(self, context, var_name, var_value):
+        # self.context contains the context that get_from_exec_frame() found the attribute in
+        if var_name not in self.var_contexts:
+            setattr(self, var_name, var_value)
+        else:
+            setattr(self.var_contexts[var_name], var_name, var_value)
+
+    # if attribute can't be found in current context recurse up to it's parent context
+    def get_from_exec_frame(self, context, var_name):
+        # first set this to None to indicate current context
+        self.context = None
+        while context is not None:
+            try:
+                var_value = getattr(context, var_name)
+                self.context = context
+                break
+            except AttributeError:
+                context = context.parent
+        self.var_contexts[var_name] = context
+        if context is None:
+            print('huh')
+        return var_value
 
     # Implements name = TOS. namei is the index of name in the attribute co_names of the code object.
     # TODO The compiler tries to use STORE_FAST or STORE_GLOBAL if possible.
     def invoke_STORE_NAME(self, val):
         tos = self.stack.pop()
         var_name = self.names[val]
-        setattr(self, var_name, tos)
+        self.set_in_exec_frame(self, var_name, tos)
         # store in globals too
-        setattr(self.globals, var_name, tos)
+        self.set_in_exec_frame(self.globals, var_name, tos)
 
     # Pushes the value associated with co_names[namei] onto the stack.
     def invoke_LOAD_NAME(self, val):
         var_name = self.names[val]
-        self.stack.append(getattr(self, var_name))
+        self.stack.append(self.get_from_exec_frame(self, var_name))
 
     # Implements TOS = TOS1 + TOS.
     def invoke_BINARY_ADD(self):
@@ -56,7 +93,6 @@ class PyByteVM:
 
 
      # Implements TOS = TOS1 - TOS.
-
     def invoke_BINARY_SUBTRACT(self):
         tos = self.stack.pop()
         tos1 = self.stack.pop()
@@ -64,7 +100,7 @@ class PyByteVM:
 
     def print_bytecode(self, code, name):
         print("-"* 60)
-        print(" "*11 + name)
+        print(" "*26 + name)
         print("-"*60)
         dis.dis(code)
         print("-"*60)
@@ -98,10 +134,10 @@ class PyByteVM:
 
         # set the keyword pairs
         for key in keyw_args:
-            setattr(func_code, key, keyw_args[key])
+            self.set_in_exec_frame(func_code, key, keyw_args[key])
         # set the positional args
         for i, item in enumerate(posn_args):
-            setattr(func_code,func_code.varnames[i], item)
+            self.set_in_exec_frame(func_code,func_code.varnames[i], item)
         # pushes the return value
         self.stack.append(func_code.execute())
 
@@ -129,10 +165,10 @@ class PyByteVM:
         self.print_bytecode(func_code, func_name)
 
         def_params.reverse()
-        func_vm = PyByteVM(func_code, self.globals)
+        func_vm = PyByteVM(module=func_code, globals_object=self.globals)
         # set the default params inside the function object
         for i, item in enumerate(def_params):
-            setattr(func_vm,func_vm.varnames[func_vm.argcount-1-i], item)
+            self.set_in_exec_frame(func_vm,func_vm.varnames[func_vm.argcount-1-i], item)
         self.stack.append(func_vm)
 
     def invoke_COMPARE_OP(self, val):
@@ -140,21 +176,19 @@ class PyByteVM:
         a = self.stack.pop()
         self.stack.append(COMPARISION[val](a,b))
 
-
     def invoke_LOAD_FAST(self, val):
-        self.stack.append(getattr(self, self.varnames[val]))
+        self.stack.append(self.get_from_exec_frame(self, self.varnames[val]))
 
     def invoke_STORE_FAST(self, val):
-        setattr(self, self.varnames[val], self.stack.pop())
+        self.set_in_exec_frame(self, self.varnames[val], self.stack.pop())
 
     def invoke_SETUP_LOOP(self, val):
-        pass
+        loop_exec_frame = PyByteVM(parent=self)
+        self.ip += val
+        loop_exec_frame.execute()
 
     def invoke_JUMP_ABSOLUTE(self, val):
         self.ip = val
-
-    def invoke_POP_BLOCK(self):
-        pass
 
     def invoke_INPLACE_ADD(self):
         # self.stack[-1] = self.stack[-2] + self.stack[-1]
@@ -167,17 +201,20 @@ class PyByteVM:
     def execute(self):
         while True:
             op_code = self.program[self.ip]
+            print(dis.opname[op_code], op_code)
             self.ip += 1
             if op_code >= dis.HAVE_ARGUMENT:
                 low = self.program[self.ip]
                 high = self.program[self.ip + 1]
                 op_arg = (high << 8) | low
                 self.ip += 2
-                getattr(self, 'invoke_'+dis.opname[op_code])(op_arg)
+                self.get_from_exec_frame(self, 'invoke_'+dis.opname[op_code])(op_arg)
             elif op_code == 83: # RETURN_VALUE
                 return self.stack.pop()
+            elif op_code == 87: # POP_BLOCK
+                return
             else:
-                getattr(self, 'invoke_'+dis.opname[op_code])()
+                self.get_from_exec_frame(self, 'invoke_'+dis.opname[op_code])()
             if self.ip >= len(self.program):
                 break
 
@@ -189,6 +226,6 @@ if __name__ == '__main__':
            source += line
     t = compile(source, input_file_name, 'exec')
     globals_object = Globals()
-    pyByte = PyByteVM(t, globals_object)
+    pyByte = PyByteVM(module=t, globals_object=globals_object)
     pyByte.print_bytecode(t, "Main")
     pyByte.execute()
